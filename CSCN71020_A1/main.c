c840e3d410cd8c17ec8c92e4231d716898c41275
Dhruv Gandhi <Dgandhi2822@conestogac.on.ca>
2022-11-04 15:00:35 -0400

    Commit #3 - Fixed the subtract function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +21,8 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:
+		subtract();
 	}
 
 }
@@ -38,10 +41,21 @@ void printOptions() {
 
 void add() {
 	double num1, num2, result;
-	printf("Enter the first value:");
+	printf("Enter the first value: ");
 	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
+	printf("Enter the second value: ");
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+//fixed subtract by copying the add function and changing it to subtraction
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value: ");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value: ");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
